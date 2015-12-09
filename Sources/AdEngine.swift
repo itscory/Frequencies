ea144cb91245aa6ec105931457f82d2daff7965f
itscory <corey@Coreys-Air.fios-router.home>
2015-12-09 11:48:07 -0500

    added AdEngine class

---
+++ Sources/AdEngine.swift
@@ -0,0 +1,13 @@
+//
+//  AdEngine.swift
+//  Frequencies
+//
+//  Created by Corey Stewart on 12/9/15.
+//  Copyright © 2015 Stewart Companies. All rights reserved.
+//
+
+import Foundation
+
+class AdEngine {
+    
+}
