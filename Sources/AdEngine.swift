71f855101c6dfb3285677da74614650a5457bf76
itscory <corey@Coreys-Air.fios-router.home>
2015-12-10 14:32:16 -0500

    worked on foundational AdEngine and DevHelper classes

--- Sources/AdEngine.swift
+++ Sources/AdEngine.swift
@@ -6,8 +6,27 @@
 //  Copyright © 2015 Stewart Companies. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import GoogleMobileAds
 
 class AdEngine {
+    let devHelper: DevHelper
+    let mainAdUnitID = "ca-app-pub-4343476466504805/1804983774"
+    var request_ads: Bool
     
+    init() {
+        devHelper = DevHelper()
+        request_ads = devHelper.request_ads
+    }
+    
+    func makeRequest(bannerView: GADBannerView, rootViewController: RootViewController) {
+        
+        print("Google Mobile Ads SDK version: " + GADRequest.sdkVersion())
+        bannerView.adUnitID = mainAdUnitID
+        bannerView.rootViewController = rootViewController
+        let bannerRequest = GADRequest()
+        bannerRequest.testDevices = [kGADSimulatorID];
+        bannerView.loadRequest(bannerRequest)
+    }
+
 }
---
+++ Sources/DevHelper.swift
@@ -0,0 +1,105 @@
+//
+//  DevHelper.swift
+//  Frequencies
+//
+//  Created by Corey Stewart on 12/9/15.
+//  Copyright © 2015 Stewart Companies. All rights reserved.
+//
+
+import Foundation
+
+
+/*
+    App Status Difference
+
+    development: logger_dev = true, logger_test = true, logger_production, panel_dev = true, panel_test = true
+
+    testing: logger_test = true,
+
+*/
+
+enum AppDevStatus {
+    case development, testing, production, production_ad_free, production_contributors_privilege
+}
+
+enum LogLevel {
+    case development, testing, production
+}
+
+struct Logger {
+    var default_log_level: LogLevel
+    var ref_class: String
+    var ref_function: String
+    private var devHelper: DevHelper
+    
+    init(default_log_level: LogLevel = LogLevel.development, ref_class: String = "", ref_function: String = "") {
+        devHelper = DevHelper()
+        self.default_log_level = default_log_level
+        self.ref_class = ref_class
+        self.ref_function = ref_function
+    }
+    
+    private func printHeader(log_level: LogLevel) {
+        
+    }
+    
+    private func printFooter(log_level: LogLevel) {
+        
+    }
+    
+    func log(body: String, var log_level: LogLevel?) {
+        if (log_level == nil) {
+            log_level = self.default_log_level
+        }
+        switch(self.devHelper.app_dev_status) {
+        case AppDevStatus.development:
+            printHeader(log_level!)
+            print(body)
+            printFooter(log_level!)
+            break
+        case AppDevStatus.testing:
+            if log_level == LogLevel.testing || log_level == LogLevel.production {
+                printHeader(log_level!)
+                print(body)
+                printFooter(log_level!)
+            }
+                break
+        case AppDevStatus.production:
+            if log_level == LogLevel.production {
+				printHeader(log_level!)
+				print(body)
+				printFooter(log_level!)
+				break
+            }
+        default:
+			print(body)
+			break
+        }
+    }
+}
+
+class DevHelper {
+    var app_dev_status: AppDevStatus
+    var log_level: LogLevel
+    
+    var panel_dev: Bool
+    var panel_test: Bool
+    
+    var track_analytics: Bool
+    
+    var request_ads: Bool
+    
+    init () {
+        app_dev_status = AppDevStatus.development
+        
+        log_level = LogLevel.development
+        
+        panel_dev = true
+        panel_test = true
+        
+        track_analytics = true
+        
+        request_ads = true
+    }
+    
+}
